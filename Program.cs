6b6ea5a1382f4c2d920ea4cfdf8f361a249bb5e5
Sudhakar-sk <msd30499@gmail.com>
2021-05-10 14:17:46 +0530

    object and class programs are updated

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 
 namespace Mynamespace{
-    class Student{
-        int id;
-        string name;
+//     class Student{
+//         int id; //data member
+//         string name; //field or data member
 
-        public static void Main(){
-            Student s1=new Student();
-            s1.id=2;
-            s1.name="sk";
-            Console.WriteLine(s1.id);
-            Console.WriteLine(s1.name);
+//         public static void Main(){
+//             Student s1=new Student();//creating object of student
+//             s1.id=2;
+//             s1.name="sk";
+//             Console.WriteLine(s1.id);
+//             Console.WriteLine(s1.name);
 
 
 
-        }
+//         }
+//     }
+// }
+/*----------------------------------------------------------------------------------------------------*/
+//C# Class Example 2: Having Main() in another class
+class student{
+    public int id;
+    public string name;
+
+}
+class TestStudent{
+    public static void Main(){
+        student s1=new student();
+        s1.id=Convert.ToInt32(Console.ReadLine());
+        s1.name=Console.ReadLine();
+
+        Console.WriteLine("Hello "+ s1.name +" your id is "+ s1.id);
+        // Console.WriteLine(s1.name);
+
     }
+}
 }
