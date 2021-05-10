43e8d715887933af4176b90abf42a9e390e2aeba
Sudhakar-sk <msd30499@gmail.com>
2021-05-10 12:59:03 +0530

    first obj and class program practice in c#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mynamespace{
+    class Student{
+        int id;
+        string name;
+
+        public static void Main(){
+            Student s1=new Student();
+            s1.id=2;
+            s1.name="sk";
+            Console.WriteLine(s1.id);
+            Console.WriteLine(s1.name);
+
+
+
+        }
+    }
+}
